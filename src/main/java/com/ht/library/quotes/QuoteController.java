b1b02effc24f93cfe85428846bfbe3e1e2e24727
hthanh <hthanh912backup@gmail.com>
2023-10-04 00:07:33 +0700

    refactor quote controller

--- src/main/java/com/ht/library/quotes/QuoteController.java
+++ src/main/java/com/ht/library/quotes/QuoteController.java
@@ -1,17 +1,15 @@
 package com.ht.library.quotes;
 
-import com.ht.library.handlers.ResponseHandler;
-import com.ht.library.review.ReviewService;
+import com.ht.library.quotes.dto.QuoteResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @Controller
@@ -22,18 +20,18 @@ public class QuoteController {
     private final QuoteService service;
 
     @GetMapping("")
-    public ResponseEntity<Object> getAllQuotes(Pageable pageable) {
-        return ResponseHandler.generateResponse("OK", HttpStatus.OK, service.getAllQuotes(pageable));
+    public ResponseEntity<List<QuoteResponse>> getAllQuotes(@PageableDefault(value = 10, page = 0) Pageable pageable) {
+        return ResponseEntity.ok(service.getAllQuotes(pageable));
     }
 
     @GetMapping("/book/{id}")
-    public ResponseEntity<Object> getAllQuotesByBookId(@PathVariable UUID id) {
-        return ResponseHandler.generateResponse("OK", HttpStatus.OK, service.getQuoteByBookId(id));
+    public ResponseEntity<List<Quote>> getAllQuotesByBookId(@PathVariable UUID id) {
+        return ResponseEntity.ok(service.getQuoteByBookId(id));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteBook(@PathVariable UUID id) {
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteBook(@PathVariable UUID id) {
         service.delete(id);
-        return ResponseHandler.generateResponse("Deleted quote id " + id + ".", HttpStatus.OK, null);
     }
 }
