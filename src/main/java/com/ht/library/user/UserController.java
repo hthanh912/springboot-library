2d892d2de896aac9347d67fb6ed124e33a3794ff
hthanh <hthanh912backup@gmail.com>
2023-10-04 00:19:30 +0700

    refactor user controller

--- src/main/java/com/ht/library/user/UserController.java
+++ src/main/java/com/ht/library/user/UserController.java
@@ -1,12 +1,11 @@
 package com.ht.library.user;
 
-import com.ht.library.handlers.ResponseHandler;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,15 +19,15 @@ public class UserController {
   private final UserService service;
 
   @GetMapping("/me")
-  public ResponseEntity<Object> getUserInfo() {
+  public ResponseEntity<UserDetailDTO> getUserInfo() {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     if (!(authentication instanceof AnonymousAuthenticationToken)) {
       String currentUserName = authentication.getName();
       Optional<User> user = service.getUser(currentUserName);
       if (user.isPresent()) {
-        return ResponseHandler.generateResponse("get user successful", HttpStatus.OK, new UserDetailDTO(user.get()));
+        return ResponseEntity.ok(new UserDetailDTO(user.get()));
       }
     }
-    return ResponseHandler.generateResponse("get user fail", HttpStatus.NOT_FOUND, null);
+    throw new UsernameNotFoundException("User not found");
   }
 }
