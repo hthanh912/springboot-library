237101cce6d296deb1c4c63693657a72ccff0277
hthanh <hthanh912backup@gmail.com>
2023-10-03 23:41:12 +0700

    refactor author controller

--- src/main/java/com/ht/library/author/AuthorController.java
+++ src/main/java/com/ht/library/author/AuthorController.java
@@ -1,15 +1,15 @@
 package com.ht.library.author;
 
+import com.ht.library.author.dto.AuthorResponse;
 import com.ht.library.exception.ResourceNotFoundException;
-import com.ht.library.handlers.ResponseHandler;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -21,15 +21,15 @@ public class AuthorController {
   private final AuthorService service;
 
   @GetMapping("")
-  public ResponseEntity<Object> getAllAuthors() {
-    return ResponseHandler.generateResponse("get authors", HttpStatus.OK, service.getAllAuthor());
+  public ResponseEntity<List<AuthorResponse>> getAllAuthors() {
+    return ResponseEntity.ok(service.getAllAuthor());
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<Object> getAuthorById(@PathVariable UUID id) {
+  public ResponseEntity<Author> getAuthorById(@PathVariable UUID id) {
     Optional<Author> author = service.getAuthorById(id);
     if (author.isPresent()){
-      return ResponseHandler.generateResponse("get author", HttpStatus.OK, author);
+      return ResponseEntity.ok(author.get());
     }
     throw new ResourceNotFoundException("Author not found");
   }
