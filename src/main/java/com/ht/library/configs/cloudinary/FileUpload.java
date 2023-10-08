d846fde430dbb9bccb5abf298e35071febd0c33b
hthanh <hthanh912backup@gmail.com>
2023-10-08 22:39:29 +0700

    update cloudinary

--- src/main/java/com/ht/library/configs/cloudinary/FileUpload.java
+++ src/main/java/com/ht/library/configs/cloudinary/FileUpload.java
@@ -3,7 +3,9 @@ package com.ht.library.configs.cloudinary;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Map;
 
 public interface FileUpload {
-  String uploadFile(MultipartFile multipartFile, String directory) throws IOException;
+  String uploadFile(MultipartFile multipartFile, String fileName, String directory) throws IOException;
+  String uploadFile(MultipartFile multipartFile, String fileName, String directory, Map<String, Object> options) throws IOException;
 }
--- src/main/java/com/ht/library/configs/cloudinary/FileUploadImpl.java
+++ src/main/java/com/ht/library/configs/cloudinary/FileUploadImpl.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -16,13 +16,24 @@ public class FileUploadImpl implements FileUpload{
   private final Cloudinary cloudinary;
 
   @Override
-  public String uploadFile(MultipartFile multipartFile, String directory) throws IOException {
+  public String uploadFile(MultipartFile multipartFile, String fileName, String directory) throws IOException {
+    return uploadFile(multipartFile, fileName, directory, null);
+  }
+
+  @Override
+  public String uploadFile(MultipartFile multipartFile, String fileName, String directory, Map<String, Object> options) throws IOException {
+    var defaultOptions =  Map.of(
+                "public_id", fileName,
+                "folder", "library/" + directory
+        );
+    Map<String, Object> mergedOption = new HashMap<>();
+    mergedOption.putAll(defaultOptions);
+    if (options != null) {
+      mergedOption.putAll(options);
+    }
     return cloudinary.uploader()
-        .upload(multipartFile.getBytes(),
-            Map.of(
-                "public_id", UUID.randomUUID().toString(),
-                "folder", "library/" + directory))
-            .get("public_id")
-        .toString();
+        .upload(multipartFile.getBytes(), mergedOption)
+          .get("public_id")
+          .toString();
   }
 }
